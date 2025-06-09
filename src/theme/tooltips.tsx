f5d29689c453dce433ee1414bc2b26415c701c05
playbooks <eric@erichubbell.com>
2025-06-09 17:00:10 -0600

    theme updates

--- src/theme/tooltips.tsx
+++ src/theme/tooltips.tsx
@@ -11,16 +11,16 @@ export const tooltip = ({ open, placement }) => ({
 });
 
 export const tooltipInner = () => ({
-	bgColor: 'bg-gray-800 dark:bg-gray-800',
-	borderRadius: 'rounded-md',
-	color: 'text-white',
+	bgColor: 'bg-gray-900 dark:bg-gray-100',
+	borderRadius: 'rounded-sm',
+	color: 'text-gray-100 dark:text-gray-700',
 	fontSize: 'text-sm',
-	spacing: 'px-4 py-3 m-1',
+	spacing: 'p-2 m-1',
 	width: 'w-auto',
 });
 
 export const tooltipArrow = () => ({
-	bgColor: 'bg-gray-800',
+	bgColor: 'bg-gray-900 dark:bg-gray-100',
 	borderRadius: 'rounded-sm',
 	rotate: 'rotate-45',
 	size: 'h-4 w-4',
